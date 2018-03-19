1af6272750be8c5305dc52178b28898376c285ac
Karim Ezzat <modosaki@gmail.com>
2018-03-19 00:22:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 function dwarfRollCall(dwarves) {
+ var text =[]
+ var i = 0
+ for (let i= 0; i < dwarves.length; i++)
+
+text.push(`${i+1}. ${dwarves[i]} `)
+return text.join("")
 }
 
+
 function summonCaptainPlanet(planeteerCalls){
+planeteerCalls.forEach(function(v,i) {
+   planeteerCalls[i] = v.toUpperCase() +'!';
+});
+return planeteerCalls
+
 }
 
 function longPlaneteerCalls(words) {
+ var x =  words.map(function(word) {
+  return word.length > 4});
+if (x[0]===true)
+ {return true}
+ else {return false}
 }
 
+
 function findTheCheese (foods) {
+var cheese = ['cheddar', 'gouda', 'camebert'];
+ var x = foods.find(function find(snacks){return snacks == 'gouda' || snacks == 'cheddar' || snacks == 'camebert'})
+  if (x===undefined){return 'no cheese!'}
+  else {return x}
 }
