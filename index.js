f7f01f39be5d6d91b17c5846d2ecd521d8ca346a
Karim Ezzat <modosaki@gmail.com>
2018-03-20 18:17:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,11 +47,11 @@ var cheese = ['cheddar', 'gouda', 'camebert'];
 }
 
 function firstb(foods){
- 
+ var x=[]
  var i = 0
  for (let i= 0; i < foods.length; i++);.
  
- if (foods[i].startsWith(b))
+ if (foods[i].startsWith("b")){return [x]
   
 
   
